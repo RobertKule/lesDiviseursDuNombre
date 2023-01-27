f9cdd05de6be5932009c8c113569d46bfbc3c9eb
Robert KULE Wa-Kangitsi <kulewakangitsirobert@gmail.com>
2023-01-27 14:18:04 +0200

    Une suite du programme diviseur du nombre

--- lesDiviseursDuNombre.py
+++ lesDiviseursDuNombre.py
@@ -18,32 +18,66 @@ def go():
 	else:
 		lresu["fg"]="red"
 		lresu["text"]="!!! Tapez d'abord une valeur svp\nCar la valeur tapée est vide"
-	
+#la fonction de la couleur
+def  CouleurBleue():#verte
+        fen["bg"]="blue"
+        l["bg"]="blue"
+        lab["bg"]="blue"
+        lresu["bg"]="blue"
+def  CouleurBlanche():#blanche
+        fen["bg"]="white"
+        l["bg"]="white"
+        lab["bg"]="white"
+        lresu["bg"]="white"
+def  CouleurGrise():#grise
+        fen["bg"]="gray"
+        l["bg"]="gray"
+        lab["bg"]="gray"
+        lresu["bg"]="gray"
+        
+def  CouleurJaune():#Jaune
+        fen["bg"]="yellow"
+        l["bg"]="yellow"
+        lab["bg"]="yellow"
+        lresu["bg"]="yellow"
+def maPhoto():
+        lb=Label(text="Ecce omo",image=PhotoImage(file="kla.png"))
+        lb.pack(side=BOTTOM)
 #ma fenetre principale
 fen=Tk()#ma fenetre
 fen["bg"]="white"
 #le cadre du programme
-cad=Frame(fen,bg="white")
+cad=Frame(fen,bg="white",height="20",width="200")
 #le label d'introduction'
-l=Label(fen,text="Welcome in this application\n",bg="white",fg="cyan",font=("Arial",14,"bold"))
+l=Label(fen,text="Welcome in this application\n",bg=fen["bg"],fg="cyan",font=("Arial",14,"bold"))
 lab=Label(cad,font=("Arial",14,"bold"),text="Entrez un nombre : ")
 #la variable de entry
 entre=Entry(cad,font=("Arial",14,"bold"),bg="cyan",fg="white")
 #le bouton de l'execution'
-executer=Button(fen,bg="green",font=("Arial",14,"bold"), text="Execute",command=go)
+executer=Button(fen,bg="green",width="10",font=("Arial",14,"bold"), text="Execute",command=go)
 #le résultant
 lresu=Label(fen,bg="white", text="",font=("Arial",12,"bold"))
-#les fausses valeurs de diviseurs
-#on ajoute nos widgets
-l.pack()
-cad.pack()#ajout qu cadre
-lab.pack(side=LEFT)
-entre.pack(side=RIGHT)
-meni=Menu(fen,bg='cyan',fg='white',font=("Arial",14,"bold"))
+#Mon menu
+meni=Menu(fen, bg="cyan")
+#le menu de la couleur
+couleur=Menu(meni,tearoff=0)
+couleur.add_command(label="Blanche",command=CouleurBlanche)#couleur verte
+couleur.add_command(label="Bleue",command=CouleurBleue)#couleur bleue
+couleur.add_command(label="Grise",command=CouleurGrise)#couleur verte
+couleur.add_command(label="Jaune",command=CouleurJaune)#couleur verte
 
-meni.add_cascade(label='\tDevelopped by Robert KULE')
+meni.add_cascade(label="Couleur",menu=couleur)
+meni.add_command(label='Developped by Robert KULE',command=maPhoto)
 fen.config(menu=meni)
-executer.pack(fill=X)
+
+
+#on ajoute nos widgets
+l.pack()#label intro
+cad.pack(side=TOP)#ajout qu cadre
+lab.pack(side=LEFT,fill=X)
+entre.pack(side=RIGHT)
+fen.resizable(width=False,height=True)
+executer.pack(side=TOP)
 lresu.pack()
 
 fen.mainloop()
